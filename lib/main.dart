6f7e1258413f6dca7524c44d2c624a69b11e6ff7
Kweayon  Clark <Kweayon@gmail.com>
2022-07-12 18:18:54 -0500

    Update

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: null,
+    ),
+  );
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("My App")),
+    );
+  }
+}
