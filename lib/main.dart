6f85f857ad45d3a0ea7389fa8628398a264e05da
Kweayon  Clark <Kweayon@gmail.com>
2022-07-12 19:17:47 -0500

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:quotes/quote.dart';
 
 void main() {
   return runApp(
     MaterialApp(
-      home: null,
+      home: quotesApp(),
     ),
   );
 }
 
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+class quotesApp extends StatefulWidget {
+  const quotesApp({Key? key}) : super(key: key);
 
   @override
-  State<Home> createState() => _HomeState();
+  State<quotesApp> createState() => _quotesAppState();
 }
 
-class _HomeState extends State<Home> {
+List<Quote> quotesList = [
+  Quote('Marcus Aurelius',
+      '“You have power over your mind - not outside events. Realize this, and you will find strength.”'),
+  Quote('Epictetus',
+      '"If you want to improve, be content to be thought foolish and stupid."'),
+  Quote('James Allen',
+      '"Men are anxious to improve their circumstances, but are unwilling to improve themselves; they therefore remain bound."'),
+  Quote('Aristotle', '“Knowing yourself is the beginning of all wisdom.”'),
+  Quote('Socretes', '“The only true wisdom is in knowing you know nothing.”'),
+];
+
+class _quotesAppState extends State<quotesApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("My App")),
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Quotes App'),
+        backgroundColor: Colors.grey[700],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+            child: Text(
+              quotesList[0].quoteText,
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Divider(
+            height: 20.0,
+            color: Colors.grey[600],
+            thickness: 1.0,
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.arrow_right
+            //size: 20.0,
+            //color: Colors.grey[600],
+            ),
+        backgroundColor: Colors.grey[600],
+      ),
     );
   }
 }
