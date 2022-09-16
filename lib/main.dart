f6080feed133d1c876a464263266cc425dadc094
Kweayon  Clark <Kweayon@gmail.com>
2022-09-16 01:16:09 -0500

    Update
    Added zen quotes API integration

--- lib/main.dart
+++ lib/main.dart
@@ -19,34 +19,37 @@ class quotesApp extends StatefulWidget {
   State<quotesApp> createState() => _quotesAppState();
 }
 
-List<Quote> quotesList = [
-  Quote(
-      author: 'Marcus Aurelius',
-      quoteText:
-          '“You have power over your mind - not outside events. Realize this, and you will find strength.”'),
-  Quote(
-      author: 'Epictetus',
-      quoteText:
-          '"If you want to improve, be content to be thought foolish and stupid."'),
-  Quote(
-      author: 'James Allen',
-      quoteText:
-          '"Men are anxious to improve their circumstances, but are unwilling to improve themselves; they therefore remain bound."'),
-  Quote(
-      author: 'Aristotle',
-      quoteText: '“Knowing yourself is the beginning of all wisdom.”'),
-  Quote(
-      author: 'Socretes',
-      quoteText: '“The only true wisdom is in knowing you know nothing.”'),
-];
+//todo: Figure out how to pass api data to app - (complete)
+// todo: fix api bugs
+
+List<Quote> quotesList = [];
 
 class _quotesAppState extends State<quotesApp> {
   int i = 0;
+
+  void setUpQuotesList() async {
+    List<Quote> listQuotes = [];
+
+    for (var i = 0; i < 5; i++) {
+      Quote q = Quote();
+      await q.fetchQuotes();
+      listQuotes.add(q);
+    }
+    setState(() {
+      quotesList = listQuotes;
+    });
+  }
 
   void delete(quote) {
     setState(() {
       quotesList.remove(quote);
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setUpQuotesList();
   }
 
   @override
