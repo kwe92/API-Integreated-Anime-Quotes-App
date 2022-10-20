7360df43c7534be2d7b0231e49364303c3d1c3f6
Kweayon  Clark <Kweayon@gmail.com>
2022-10-20 15:38:35 -0500

    Inital Commit
    Added tests for the Anime Repository

---
+++ test/src/features/quotepage/data/anime_chan_repository_test.dart
@@ -0,0 +1,22 @@
+@Timeout(Duration(seconds: 10))
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:quotes/src/features/quotepage/data/anime_chan_repository.dart';
+import 'package:quotes/src/features/quotepage/domain/quote.dart';
+
+void main() {
+  late final AnimeChanRepository animeChanRepository;
+  setUp(() {
+    animeChanRepository = const AnimeChanRepository();
+  });
+  group('Anime Chan Repository', () {
+    test('fetchQuoteList(length: 1) returns [Quote]', () async {
+      final result = await animeChanRepository.fetchQuoteList(length: 1);
+      expect(result, [const Quote(anime: '', quoteText: '', character: '')]);
+    });
+
+    test(
+        'fetchQuoteListByTitle(Avatar) returns [Quote(), Quote(), Quote(), Quote(), Quote(), Quote(), Quote(), Quote(), Quote(), Quote()]',
+        () {});
+  });
+}
