faba1b5953ea9957ec273f51572d0433d1eec132
Kweayon  Clark <Kweayon@gmail.com>
2022-10-20 15:37:53 -0500

    Update
    Removed old commented out code

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quotes/src/features/quotepage/presentation/quotes_screen/quote_screen.dart';
-import 'package:quotes/src/routing/app_router.dart';
 
 void main() {
   return runApp(
     const ProviderScope(
-        child: MaterialApp(
-      home: QuotesScreen(),
-    )
-        // MaterialApp.router(
-        //   routerDelegate: goRouter.routerDelegate,
-        //   routeInformationParser: goRouter.routeInformationParser,
-        //   routeInformationProvider: goRouter.routeInformationProvider,
-        //   debugShowCheckedModeBanner: false,
-        // ),
-        ),
+      child: MaterialApp(
+        home: QuotesScreen(),
+      ),
+    ),
   );
 }
